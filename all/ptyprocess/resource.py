8d04ba6f592041b0c8b582b59c9ef2658ec8bf5f
Randy Lai <randy.cs.lai@gmail.com>
2018-06-16 14:36:27 -0400

    use subprocess

--- all/ptyprocess/resource.py
+++ all/ptyprocess/resource.py
@@ -1,17 +1,11 @@
-import ctypes
-import ctypes.util
+import subprocess
 
 
-class StructRLimit(ctypes.Structure):
-    _fields_ = [('rlim_cur', ctypes.c_ulong), ('rlim_max', ctypes.c_ulong)]
+RLIMIT_NOFILE = None
 
 
-RLIMIT_NOFILE = 8
-
-
-def getrlimit(handler):
-    limits = StructRLimit()
-    libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
-    libc.getrlimit(handler, ctypes.byref(limits))
-
-    return (limits.rlim_cur, limits.rlim_max)
+def getrlimit(resource):
+    if resource == RLIMIT_NOFILE:
+        soft_limit = int(subprocess.check_output(['ulimit', '-Sn']))
+        hard_limit = None
+        return (soft_limit, hard_limit)
