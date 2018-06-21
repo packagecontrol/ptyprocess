c6c82bdc2242c68f4af25bfc2c62e7426390e83f
Randy Lai <randy.cs.lai@gmail.com>
2018-06-21 09:46:34 -0400

    use sh

--- all/ptyprocess/resource.py
+++ all/ptyprocess/resource.py
@@ -1,11 +1,11 @@
 import subprocess
 
 
-RLIMIT_NOFILE = None
+RLIMIT_NOFILE = 7
 
 
 def getrlimit(resource):
     if resource == RLIMIT_NOFILE:
-        soft_limit = int(subprocess.check_output(['ulimit', '-Sn']))
+        soft_limit = int(subprocess.check_output(['sh', '-c', 'ulimit -Sn']))
         hard_limit = None
         return (soft_limit, hard_limit)
